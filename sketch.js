6f2092b79b5875b5b26d7c85c641e636d3376b30
Aurore Dimech <aurore.dimech@edu.devinci.fr>
2025-03-17 23:33:04 +0100

    mid front

--- sketch.js
+++ sketch.js
@@ -1,39 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    let classifier;
     const URL = "./model/";
     
-    let model, webcam, labelContainer, maxPredictions;
+    let model, webcam;
     
     async function init() {
         try {
             const modelURL = URL + "model.json";
             const metadataURL = URL + "metadata.json";
     
-            // Load the model
             model = await tmImage.load(modelURL, metadataURL);
             maxPredictions = model.getTotalClasses();
     
-            // Setup webcam
             const flip = true;
-            webcam = new tmImage.Webcam(200, 200, flip);
+            webcam = new tmImage.Webcam(300, 300, flip);
             await webcam.setup();
             await webcam.play();
     
-            // Start the loop
             window.requestAnimationFrame(loop);
     
-            // Append the webcam canvas to the container
             document.querySelector("#webcam-container").appendChild(webcam.canvas);
-        
-            labelContainer = document.getElementById("label-container");
-    
-            // Create label elements
-            for (let i = 0; i < maxPredictions; i++) { 
-                labelContainer.appendChild(document.createElement("div"));
-            }
-    
-            console.log("Webcam and model are ready.");
         } catch (error) {
             console.error("Error loading model or initializing webcam: ", error);
         }
@@ -48,10 +33,6 @@ document.addEventListener('DOMContentLoaded', () => {
     async function predict() {
         const prediction = await model.predict(webcam.canvas);
         latestPrediction = prediction; 
-        for (let i = 0; i < maxPredictions; i++) {
-            const classPrediction = prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-            labelContainer.childNodes[i].innerHTML = classPrediction;
-        }
     }
 
     let turn = 1;
@@ -63,12 +44,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const firstPlayerScoreDisplay = document.querySelector('#first-player-score');
     const secondPlayerScoreDisplay = document.querySelector('#second-player-score');
     const history = document.querySelector('#history');
+    const winnerContainer = document.querySelector('.winner-container');
+    const winnerAnnouncer = document.querySelector('.winner-announcer');
 
     firstPlayerScoreDisplay.innerText = firstPlayerScore;
     secondPlayerScoreDisplay.innerText = secondPlayerScore;
 
+    const firstPlayerCard = document.querySelector('#first-player-card')
+    const secondPlayerCard = document.querySelector('#second-player-card')
+
+    turn % 2 == 1 ? firstPlayerCard.classList.add('background-red') : secondPlayerCard.classList.add('background-yellow');
+
     function captureImage() {
-        const captureButton = document.getElementById('capture-button');
+        const captureButton = document.querySelector('#capture-button');
         let isCapturing = false;
 
         captureButton.addEventListener('click', () => {
@@ -91,9 +79,10 @@ document.addEventListener('DOMContentLoaded', () => {
             setTimeout(async () => {
                 await predict();
                 const closestPrediction = latestPrediction.reduce((prev, current) => (prev.probability > current.probability) ? prev : current);
-                console.log("Closest Prediction: ", closestPrediction);
                 if(turn % 2 == 1){
                     firstPlayerMove = closestPrediction.className;
+                    secondPlayerCard.classList.toggle('background-yellow');
+                    firstPlayerCard.classList.toggle('background-red');
                 } else {
                     secondPlayerMove = closestPrediction.className;
 
@@ -102,13 +91,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
                     getWinner(firstPlayerMove, secondPlayerMove);
 
+                    winnerContainer.classList.toggle('hidden')
+
+                    secondPlayerCard.classList.toggle('background-yellow');
+                    firstPlayerCard.classList.toggle('background-red');
                     const historyEntry = document.createElement('div');
                     historyEntry.className = 'player number 1';
                     historyEntry.innerHTML = `
                         <img src="./assets/${firstPlayerMove}.png" alt="${firstPlayerMove} illustration" class="little-illustration">
                         <img src="./assets/${secondPlayerMove}.png" alt="${secondPlayerMove} illustration" class="little-illustration">
                     `;
-                    history.insertBefore(historyEntry, history.firstChild); // Insert as the first child
+                    history.insertBefore(historyEntry, history.firstChild);
+
+
+                    setTimeout(() => {
+                        firstPlayerMoveDisplay.src = `./assets/undefined.png`; 
+                        secondPlayerMoveDisplay.src = `./assets/undefined.png`;
+                        winnerContainer.classList.toggle('hidden')
+                    }, 3000)
                 }
                 turn++;
                 isCapturing = false;
@@ -118,22 +118,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const getWinner = (firstPlayerMove, secondPlayerMove) => {
         if(firstPlayerMove == secondPlayerMove){
+            winnerAnnouncer.innerText = 'Egalité !'
             return;
         }
 
         if (firstPlayerMove == 'pierre') {
-            secondPlayerMove == 'feuille' ? secondPlayerScore++ : firstPlayerScore++;
+            secondPlayerMove == 'feuille' ? winner = 'Joueur 2': winner = 'Joueur 1' ;
         } else if (firstPlayerMove == 'feuille') {
-            secondPlayerMove == 'ciseaux' ? secondPlayerScore++ : firstPlayerScore++;
+            secondPlayerMove == 'ciseaux' ? winner = 'Joueur 2': winner = 'Joueur 1';
         } else {
-            secondPlayerMove == 'pierre' ? secondPlayerScore++ : firstPlayerScore++;
+            secondPlayerMove == 'pierre' ? winner = 'Joueur 2': winner = 'Joueur 1' ;
         }
+
+        winner == 'Joueur 2' ? secondPlayerScore++ : firstPlayerScore++ ;
+        console.log(winner, firstPlayerScore, secondPlayerScore)
 
         firstPlayerScoreDisplay.innerText = firstPlayerScore;
         secondPlayerScoreDisplay.innerText = secondPlayerScore;
+        winnerAnnouncer.innerText = `${winner} gagne !`
     };
     
-    // Initialize the app
     init();
     captureImage();
 });
